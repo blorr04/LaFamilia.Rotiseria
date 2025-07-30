1d962173fdb22ee7a6afe8357b93355c1ea17b90
LaFamilia.Rotiseria <lafamilia.rotiseria2@gmail.com>
2025-07-30 19:11:18 -0300

    Arreglar configuración de API y Vercel

--- api/menu.js
+++ api/menu.js
@@ -1,4 +1,6 @@
 // /api/menu.js
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 module.exports = function handler(req, res) {
   const menuItems = [
